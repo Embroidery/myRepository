fc9d81fdbe458a00ec5e4a040cf6a988941890d8
yangyong <yang1985yong@163.com>
2013-01-24 02:24:04 +0800

    add test project

---
+++ test/src/com/yy/Connsql.java
@@ -0,0 +1,13 @@
+package com.yy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Connsql {
+		
+	public static void main(String[] args) {
+		System.out.println("test remote github");
+	}
+}
